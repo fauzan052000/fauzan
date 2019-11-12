684068faada753005933d990469002f8fb8aee57
fauzan052000 <57649910+fauzan052000@users.noreply.github.com>
2019-11-12 10:05:51 +0700

    Add files via upload

---
+++ daftar.php
@@ -0,0 +1,97 @@
+<html>
+	<head>
+		<title></title>
+		<link rel="stylesheet" href="desain(1).css">
+	</head>
+	<body>
+		<div class="header">
+			<h1> toko koko max</h1>
+		</div>
+
+
+
+		<div class="menu">
+			<ul>
+				<li>Halaman Depan</li>
+				<li>Informasi</li>
+				<li>Kontak</li>
+				<li><a href="login.php
+					">Login</li>
+				<li><a href="daftar.php">daftar</li>
+			</ul>
+		</div>
+
+
+
+		<div class="content">
+			
+
+			<div class="bagiankiri">
+				
+<form action="">
+	<table>
+		<tr>
+			<td>username</td>
+			<td><input type="text" name="username"></td>
+		</tr>
+		<tr>
+			<td>nama lengkap</td>
+			<td><input type="text" name="nama lengkap"></td>
+		</tr>
+		<tr>
+			<td>jenis kelamin</td>
+			
+			     <td>
+			     	<select name="jk">
+			        <option value="l">laki-laki</option>
+			          <option value="l">perempuan</option>
+		</tr>
+		<tr>
+			<td>tanggal lahir</td>
+			<td><input type="text" name="tanggal lahir"></td>
+		</tr>
+		<tr>
+			<td>alamat</td>
+			<td><input type="text" name="alamat"></td>
+		</tr>
+		<tr>
+			<td>email</td>
+			<td><input type="text" name="email"></td>
+		</tr>
+		<tr>
+			<td>hp</td>
+			<td><input type="text" name="hp"></td>
+		</tr>
+		<tr>
+			<td>password</td>
+			<td><input type="text" name="password"></td>
+		</tr>
+		<tr>
+			<td>jenis kelamin</td>
+			<td><input type="text" name="jenis kelamin"></td>
+		</tr>
+		<tr>
+			<td>status</td>
+			<td>
+				<input type="radio" name="status"value="sudah">
+				sudah menikah
+				<input type="radio" name="status"value="belum">
+				belum menikah
+				</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit"value="daftar"></td>
+			</tr>
+		</table>
+	</form>
+
+
+
+	    
+
+			
+
+
+
+
---
+++ login.php
@@ -0,0 +1,16 @@
+<form action="">
+	<table>
+		<tr>
+			<td>username</td>
+			<td><input type="text" name="username"></td>
+		</tr>
+		<tr>
+			<td>password</td>
+			<td><input type="text" name="password"></td>
+		</tr>
+		<tr>
+				<td></td>
+				<td><input type="submit"value="daftar"></td>
+			</tr>
+		</table>
+	</form>
